f62894987736e38bc94c6fe03ac24f2ce4059d6e
JL-Z <zhangjl_0912@163.com>
2018-08-25 09:59:56 +0800

    Commit

--- lagou_spider/lagou_spider/settings.py
+++ lagou_spider/lagou_spider/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'lagou_spider.spiders'
 #USER_AGENT = 'lagou_spider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
